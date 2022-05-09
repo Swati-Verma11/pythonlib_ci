02bdf84903cc5e06e60c87e7021eaca861e06226
Swati-Verma11 <2018258@iiitdmj.ac.in>
2022-05-09 16:03:37 +0530

    Updated src

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Find the fibonacci
 A starter project to show how to set up and use automated testing in Python
 
-![Tests](https://github.com/Swati-Verma11/Testing/actions/workflows/tests.yml/badge.svg)
+<!-- ![Tests](https://github.com/Swati-Verma11/Pythonlib_ci/actions/workflows/tests.yml/badge.svg) -->
--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -5,9 +5,10 @@ from fibonacci.fibonacci import fibonacci
 
 class TestFibo(unittest.TestCase):
     def test_fibonacci_1(self):
-        self.assertEqual(fibonacci(1) ,1)
+        self.assertEqual(fibonacci(1), 1)
     # def test_fibonacci_2(self):
     #     self.assertEqual(fibonacci(2) , 2)
 
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main()    
