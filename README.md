8b92bdd0b01847cde2bac29da79685ef6e6944ce
Swati-Verma11 <2018258@iiitdmj.ac.in>
2022-05-09 13:45:18 +0530

    Starter Python lib without git actions

---
+++ README.md
@@ -0,0 +1,4 @@
+# Find the fibonacci
+A starter project to show how to set up and use automated testing in Python
+
+![Tests](https://github.com/Swati-Verma11/Testing/actions/workflows/tests.yml/badge.svg)
---
+++ tests/test_fibonacci.py
@@ -0,0 +1,13 @@
+import unittest
+# import pytest
+from fibonacci.fibonacci import fibonacci
+
+
+class TestFibo(unittest.TestCase):
+    def test_fibonacci_1(self):
+        self.assertEqual(fibonacci(1) ,1)
+    # def test_fibonacci_2(self):
+    #     self.assertEqual(fibonacci(2) , 2)
+
+if __name__ == '__main__':
+    unittest.main()
